b305ebcd0b033eca4c38c0ecb9d69d251ef99518
HoffsMH <hecker.matthew@gmail.com>
2018-02-04 09:24:43 -0500

    added a pattern in the collector that protects against nil

--- test/word_collector_test.exs
+++ test/word_collector_test.exs
@@ -24,4 +24,11 @@ defmodule WordCollectorTest do
 
     assert @wc.collect(model[:trie]) === ["a", "ba", "bat", "batter"]
   end
+
+  test "collect with more complicated model and nothing to collect" do
+    model = CompleteMe.populate()
+
+    assert @wc.collect(model[:trie]) === []
+  end
+
 end

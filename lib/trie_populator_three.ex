8faa543b05ba26a17b4a5e4cc1df94000b7e5d60
HoffsMH <hecker.matthew@gmail.com>
2018-04-29 17:03:50 -0400

    filled out the class as a clone of tp2

---
+++ lib/trie_populator_three.ex
@@ -0,0 +1,17 @@
+defmodule TriePopulatorThree do
+  @ti TrieInserter
+  @tm TrieMerger
+  @p Parallel
+
+  def populate(words \\ "")
+  def populate(word_list) when is_list(word_list) do
+    word_list
+    |> @p.map(&@ti.insert/1)
+    |> Enum.reduce(%{}, &@tm.merge/2)
+  end
+
+  def populate(words_text) when is_binary(words_text) do
+    String.split(words_text, "\n")
+    |> populate
+  end
+end

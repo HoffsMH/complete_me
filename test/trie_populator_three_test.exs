c3078462f7e28c267447e4cc007a25f3eaa5e2b5
HoffsMH <hecker.matthew@gmail.com>
2018-04-29 16:44:48 -0400

    adds trie pop 3 unit tests, nice and slow

---
+++ test/trie_populator_three_test.exs
@@ -0,0 +1,73 @@
+defmodule TriePopulatorTwoTest do
+  use ExUnit.Case, async: true
+  @tp2 TriePopulatorTwo
+
+  test "populate with nothing" do
+    assert @tp2.populate() === %{}
+  end
+
+  test "populate with empty string" do
+    assert @tp2.populate("") === %{}
+  end
+
+  test "populate with a" do
+    expected = %{a: %{value: "a"}}
+
+    assert @tp2.populate("a") === expected
+  end
+
+  test "populate with b" do
+    expected = %{b: %{value: "b"}}
+
+    assert @tp2.populate("b") === expected
+  end
+
+  test "populate with a\nb" do
+    expected = %{a: %{value: "a"}, b: %{value: "b"}}
+
+    assert @tp2.populate("a\nb") === expected
+  end
+
+  test "populate with [a,b]" do
+    expected = %{a: %{value: "a"}, b: %{value: "b"}}
+
+    assert @tp2.populate(["a", "b"]) === expected
+  end
+
+  test "populate with medium word list" do
+    with {:ok, medium_text} <- medium_word_list() do
+      trie = @tp2.populate(medium_text)
+
+      result = trie[:v][:e][:t][:u][:s][:t][:value]
+
+      assert result === "vetust"
+
+      result = trie[:f][:a][:s][:t][:h][:o][:l][:d][:value]
+
+      assert result === "fasthold"
+    end
+  end
+
+  @tag :skip
+  test "populate with large word list" do
+    with {:ok, large_text} <- large_word_list() do
+      trie = @tp2.populate(large_text)
+
+      result = trie[:v][:e][:t][:u][:s][:t][:value]
+
+      assert result === "vetust"
+
+      result = trie[:f][:a][:s][:t][:h][:o][:l][:d][:value]
+
+      assert result === "fasthold"
+    end
+  end
+
+  def medium_word_list do
+    File.read("./test/medium.txt")
+  end
+
+  def large_word_list do
+    File.read("/usr/share/dict/words")
+  end
+end

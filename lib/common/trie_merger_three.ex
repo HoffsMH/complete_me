f129acede0e89e6c51c9fe0250557b300d6d0562
HoffsMH <hecker.matthew@gmail.com>
2019-05-04 17:13:15 -0500

    added a trieMergerThree that is just a wrapper for DeepMerge
    https://github.com/PragTob/deep_merge
    doesnt seem to perform much better

---
+++ lib/common/trie_merger_three.ex
@@ -0,0 +1,8 @@
+defmodule TrieMergerThree do
+  def merge(nil, trie), do: trie
+
+  def merge(trie_one, trie_two) when is_map(trie_two) do
+    DeepMerge.deep_merge(trie_one, trie_two)
+  end
+
+end
